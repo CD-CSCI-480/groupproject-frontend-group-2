f184283296fec8507282313fe2d2a8968ca87b9e
cZAlpha <nbklaus21@students.desu.edu>
2024-04-16 14:01:32 -0400

    Added blank settings screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainMenu from './src/app/mainmenu';
 import AuthScreen from './src/app/authscreen';
+import SettingsScreen from './src/app/settingsscreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,14 +21,17 @@ export default function App() {
               iconSource = focused ? require('./assets/white_home_icon.png') : require('./assets/white_home_icon.png');
             } else if (route.name === 'AuthScreen') {
               iconSource = focused ? require('./assets/white_accounts_icon.png') : require('./assets/white_accounts_icon.png');
+            } else if (route.name === 'SettingsScreen') {
+              iconSource = focused ? require('./assets/white_settings_icon.png') : require('./assets/white_settings_icon.png');
             }
 
-            return <Image source={iconSource} style={{ width: 24, height: 24, tintColor: color }} />;
+            return <Image source={iconSource} style={{ width: 30, height: 30, tintColor: color }} />;
           },
           tabBarStyle: {
             backgroundColor: 'black', // Change to black
             borderTopWidth: 0,
             elevation: 0, // remove shadow on Android
+            height: 100, // Increase height of the tabBar
           },
         })}
         tabBarOptions={{
@@ -37,6 +41,7 @@ export default function App() {
       >
         <Tab.Screen name='MainMenu' component={MainMenu} options={{ headerShown: false, tabBarLabel: '' }} />
         <Tab.Screen name='AuthScreen' component={AuthScreen} options={{ headerShown: false, tabBarLabel: ''}} />
+        <Tab.Screen name='SettingsScreen' component={SettingsScreen} options={{ headerShown: false, tabBarLabel: ''}} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ src/app/settingsscreen.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { Link } from 'expo-router';
+import Button from '../components/Button';
+import IconButton from '../components/IconButton'; // Import the IconButton component
+import QuantumCheckersTitle from '../components/QuantumCheckersTitle';
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <ImageBackground
+      source={require('../../assets/background.jpg')}
+        style={styles.container}
+      >
+        <View style={styles.container}>
+          {/* Title */}
+          <View style={styles.title}>
+            <QuantumCheckersTitle />
+          </View>
+          
+          {/* Menu Buttons */}
+          <View style={styles.buttons}>
+            <Text>Testing Settings Screen</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+export default SettingsScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between', // Space evenly between content
+    paddingTop: 30, // Add margin to the top of the container
+    paddingHorizontal: 20, // Add horizontal padding
+  },
+  title: {
+    flex: 0.3, // Each part takes up an equal amount of space
+    justifyContent: 'center', // Center the title vertically
+    paddingBottom: 30, 
+  },
+  imageContainer: {
+    flex: 1, // Each part takes up an equal amount of space
+    justifyContent: 'center', // Center the image vertically
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  buttons: {
+    flex: 1, // Each part takes up an equal amount of space
+    justifyContent: 'center', // Center the buttons vertically
+    alignItems: 'center', // Center buttons horizontally
+  },
+  button: {
+    paddingBottom: 15, // Add vertical margin between buttons
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center', // Center the images horizontally
+    paddingBottom: 30, // Add padding to the bottom of the icons container
+  },
+});
